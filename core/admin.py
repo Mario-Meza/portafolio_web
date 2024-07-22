84ac1f557a9bb2e5b9ce7e1d9df2451fa3f09972
mario.mesa.sosa <meza.meza.alt@gmail.com>
2024-07-22 14:21:31 -0600

    Add tags

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,19 @@
 from .models import Project
+from .models import Tag
 from django.contrib import admin
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    search_fields = ('title',)
+    list_filter = ('tags',)
 
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Tag, TagAdmin)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from froala_editor.fields import FroalaField
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name='Titulo')
@@ -9,12 +16,13 @@ class Project(models.Model):
         plugins=(
             'font_size', 'font_family', 'align',
             'code_view', 'colors', 'draggable',
-            'emoticons', 'inline_style','lists', 'paragraph_format','quote', 'file', 'image_manager', 'image'),
+            'emoticons', 'inline_style', 'lists', 'paragraph_format', 'quote', 'file', 'image_manager', 'image'),
         theme='dark', verbose_name='Descripción')
     image = models.ImageField(verbose_name='Imagen', upload_to='projects', null=True, blank=True)
     link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creacion')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualizacion')
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
         verbose_name = 'projecto'
