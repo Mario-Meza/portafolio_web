a335be9cbd8f16406aacbdd5073b829a2a096be0
mario.meza <meza.meza.alt@gmail.com>
2025-03-26 11:45:37 -0600

    Cambio de colores de Menu

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export const MenuComponent = () => {
     const {activeSection} = useActiveSection();
-    const [codeText, setCodeText] = useState('("Hello, World!")');
+    const [codeText, setCodeText] = useState('');
     const navLinks = [
         { name: 'About', path: '#about'},
         { name: 'Experience', path: '#experience'},
@@ -27,7 +27,7 @@ export const MenuComponent = () => {
                                 {navLinks.map((item, index) => (
                                     <li key={index} className="flex items-center">
                                         <a href={item.path}
-                                           className={`text-gray-200 accent-gray-400 hover:text-green-300 dark:hover:text-green-300 ${activeSection === item.path.replace('#', '') ? 'text-indigo-300 dark:text-green-300 font-medium' : ''}`}>
+                                           className={`text-gray-200 accent-gray-400 hover:text-violet-300 dark:hover:text-violet-300 ${activeSection === item.path.replace('#', '') ? 'text-indigo-300 dark:text-fuchsia-400 font-medium' : ''}`}>
                                             {item.name}
                                         </a>
                                     </li>
@@ -39,7 +39,7 @@ export const MenuComponent = () => {
                         <h1 className="hero-title">Frontend Developer</h1>
                             <div className="font-mono bg-white/5 p-4 rounded-lg border border-white/10">
                                 <div className="hero-container">
-                                    <span className="hero-return">console.log</span>
+                                    <span className="hero-return">Hello 👋, I'm Mario</span>
                                     <span
                                         className="hero-hello-world"
                                         contentEditable={true}
